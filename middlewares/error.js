fcd0f5565b9cc124a3ca02745849399be0e429d5
basit <basit>
2024-01-27 13:02:22 +0530

    last banckend commit

---
+++ middlewares/error.js
@@ -0,0 +1,20 @@
+class ErrorHandler extends Error{
+constructor(message,statusCode){
+super(message) //parent ie, Error
+this.statusCode = statusCode
+}
+}
+
+
+export const errorMiddleware = (err,req,res,next)=>{
+  err.message = err.message || "Internal Server Error"
+  err.statusCode = err.statusCode || 500
+  return res.status(err.statusCode).json({
+    status : false,
+    message: err.message
+   })
+}
+
+
+
+export default ErrorHandler
